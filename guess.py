207e55385cebe15565e5da1f7b776bb17d8deeaf
Vi Nguyen <phuongvinguyen2000@gmail.com>
2022-12-29 02:27:39 +0100

    Uploading stuffs

---
+++ guess.py
@@ -0,0 +1,32 @@
+import random
+
+def askForGuess():
+    while True:
+        guess = input('>') #Enter the guess
+
+        if guess.isdecimal():
+            return int(guess) #Convert string guess to an integer
+        print('Please enter a number between 1 and 100')
+
+print()
+secretNumber = random.randint(1,100) #Select a random number
+
+print('I am thinking of a number between 1 and 100')
+
+for i in range (10): #Give the player 10 guesses
+    print('You have {} guesses left. Take a guess'.format(10-i))
+    guess = askForGuess()
+    if guess == secretNumber:
+        break
+
+    #Offer a hint:
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    if guess > secretNumber:
+        print('Your guess is too high')
+
+#Reveal the results
+if guess == secretNumber:
+    print('Yay! You got it!')
+else:
+    print('Game over!')
